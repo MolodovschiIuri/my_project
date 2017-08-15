86c73a0aef56f2e9261629f796220a2553162509
Iuri <molodovschi.iury@gmail.com>
2017-08-15 11:22:57 +0300

    First commit

--- check.php
+++
@@ -1,8 +0,0 @@
-<?php
-sleep(3);
-    if ($_POST['name'] == "Admin"){
-        echo "fail";
-    }else{
-        echo $_POST['name'];
-    }
-?>
--- content.php
+++
@@ -1,4 +0,0 @@
-<?php
-    sleep(3);
-    echo "Данные : строка - ".$_POST['name'].", цифра - ".$_POST['number'];
-?>
